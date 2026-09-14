470cc9f08de745a0fa358d56e3f04d43c4f8030a
Your Name <youremail@example.com>
2026-09-14 11:57:09 +0500

    Add Python code

---
+++ code.py
@@ -0,0 +1,11 @@
+v1=float(input("Enter V1: "))
+v2=float(input("Enter V2: "))
+operator=input("Enter Operator (+, -, *, /): ")
+if operator=='+':
+    print("Result: ", v1+v2)
+elif operator=='-':
+    print("Result: ", v1-v2)
+elif operator=='*':
+    print("Result: ", v1*v2)
+elif operator=='/':
+    print("Result: ", v1/v2)
